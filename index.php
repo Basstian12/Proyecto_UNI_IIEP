20b5fa5d6a8560b84a1590bad18c57b0691aeebc
Sebastian <89953271+Basstian12@users.noreply.github.com>
2022-01-31 16:24:59 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IIEP | Instituto intercultural de estudios profesionales</title>
+    <link class="lhead" rel="icon" href="img/logo1.png">
+    <!-- estilos hasta mision y vision -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- estilos de noticias -->
+    <link rel="stylesheet" href="css/noticias.css">
+    <!-- iconos de bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <header>
+        <div class="logo">
+            <h3>INSTITUTO INTERCULTURAL DE ESTUDIOS PROFESIONALES</h3>
+            <img src="img/logo2.png" alt="logo2">
+            <h3 class="h3">¡AGENTES DE CAMBIO, POR EXCELENCIA!</h3>
+        </div><br>
+
+        <div class="referencias">
+            <div class="bcontacto">
+                <div class="bcontacto-tel">
+                    <i class="bi bi-telephone-fill bcontacto-tel"></i>
+                </div>
+                <div class="bcontacto-num">
+                    <a href="#">CONTACTO<br>9614562573</a>
+                </div>
+            </div>
+            <div class="breferencias">
+                <div class="breferencias-title">
+                    <h1>IIEP</h1>
+                </div>
+                <div class="breferencias-link">
+                    <a href="index.php"><i class="bi bi-twitter iconos"></i></a>
+                    <a href="https://m.facebook.com/IIEPCHIAPAS/"><i class="bi bi-facebook iconos"></i></a>
+                    <a href="#"><i class="bi bi-instagram iconos"></i></a>
+                    <a href="mailto:iiep.educ@gmail.com"><i class="bi bi-envelope-fill iconos"></i></a>
+                </div>
+
+            </div>
+        </div>
+
+
+    </header>
+    <!-- barra de navegacion -->
+
+    <nav class="nav">
+        <div class="nava">
+            <a href="#">Conócenos</a>
+            <a href="#">Oferta educativa</a>
+        </div>
+    </nav>
+    <i class="bi bi-list"></i>
+
+    <aside>
+        <div class="noticias">
+            <div class="title">
+                <h2>Noticias recientes</h2>
+            </div>
+            <div>
+                <p><span class="date">Martes 23 de noviembre del 2021</span></p>
+                <h4>
+                    <a href="#" class="message">Se llevo a cabo una reunion con los padres de familia</a>
+                </h4>
+            </div><br>
+        </div>
+    </aside>
+
+
+
+    <main>
+        <div class="fundacion">
+            <p>
+                El IIEP es un Instituto que nace en el 2021, con el propósito de brindar nuevas alternativas de
+                formación profesional y de capacitación en diversas áreas del conocimiento, formando ¡agentes de cambio
+                por excelencia! Aportando al crecimiento personal, profesional y humano, con una visión transformadora
+                de la educación del Siglo XXI.
+                Brindar una formación continua en las diversas áreas de formación profesional basado en los principios
+                de identidad y diversidad cultural que permite enfrentar los cambios del siglo XXI.
+            </p>
+        </div>
+
+        <div class="divmv">
+            <div class="mision">
+                <h1>MISION</h1>
+                <p>
+                    Formar profesionales de excelencia académica con sentido humanista que contribuya a la formación
+                    continua e integral de la persona, capaces de responder a las
+                    exigencias del siglo XXI; y ser reconocidos como verdaderos agentes de cambio por excelencia.
+                </p>
+            </div>
+            <div class="vision">
+                <h1>VISION</h1>
+                <p>
+                    Ser un instituto reconocido a nivel local y nacional por la calidad e innovación de sus programas
+                    académicos bajo una visión crítica y transformadora.
+                </p>
+            </div>
+        </div>
+    </main>
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet exercitationem, sit corporis,
+            necessitatibus beatae delectus fugiat voluptatem quos saepe iusto mollitia ut illum culpa. Veniam autem
+            laboriosam vero reiciendis impedit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet exercitationem, sit corporis,
+            necessitatibus beatae delectus fugiat voluptatem quos saepe iusto mollitia ut illum culpa. Veniam autem
+            laboriosam vero reiciendis impedit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet exercitationem, sit corporis,
+            necessitatibus beatae delectus fugiat voluptatem quos saepe iusto mollitia ut illum culpa. Veniam autem
+            laboriosam vero reiciendis impedit.</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet exercitationem, sit corporis,
+            necessitatibus beatae delectus fugiat voluptatem quos saepe iusto mollitia ut illum culpa. Veniam autem
+            laboriosam vero reiciendis impedit.</p>
+    </section>
+
+
+    <footer>
+        <div class="footer_main">
+            <h4>INSTITUTO INTERCULTURAL DE ESTUDIOS PROFESIONALES</h4>
+            <p>&copy;2021 Derechos reservados</p>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
---
+++ otro.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- iconos de bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div class="header">
+            <div class="logo">
+                <h1>IIEP
+                </h1>
+                <hr> 
+                <p>Instituto intercultural de estudios profesionales</p>
+            </div>
+            <div class="referencias">
+                <div class="telefono">
+                    <div class="logotel">
+                        <i class="bi bi-telephone-fill bcontacto-tel"></i>
+                    </div>
+                    <div class="numtel">
+                        <h3>CONTACTO<br>9614562573</h3>
+                    </div>
+                </div>
+                <div class="redes">
+                    <div class="nombrescool">
+                        <h2>IIEP</h2>
+                    </div>
+                    <div class="redes_link">
+                        <a href="#"><i class="bi bi-twitter iconos"></i></a>
+                        <a href="https://m.facebook.com/IIEPCHIAPAS/"><i class="bi bi-facebook iconos"></i></a>
+                        <a href="#"><i class="bi bi-instagram iconos"></i></a>
+                        <a href="mailto:iiep.educ@gmail.com"><i class="bi bi-envelope-fill iconos"></i></a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="nav">
+        <nav>
+            <ul>
+                <li>
+                    <a href="">INICIO</a>
+                </li>
+                <li>
+                    <a href="">QUIENES SOMOS</a>
+                </li>
+                <li>
+                    <a href="">CONTACTO</a>
+                </li>
+
+            </ul>
+        </nav>
+    </div>
+
+</body>
+
+</html>
